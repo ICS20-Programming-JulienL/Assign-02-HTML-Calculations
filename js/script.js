1561d8dd282c7231585f4d3efc7534ce0ea1469d
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-03-29 20:22:54 +0000

    Finish most of the website

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 // Copyright (c) 2023 Julien Lamoureux inc. All rights reserved
 //
 // Created by: Julien Lamoureux
-// Created on: March 20 2023
+// Created on: March 29 2023
 // This file contains the JS functions for index.html
 
 "use strict";
 
 function enterClicked() {
-  // get the user work hours
+  // get the length from user input
   let length= parseFloat(document.getElementById("prism-length").value)
 
-	// get the hourly pay from the user
+	// get the height from user input
   let height= parseFloat(document.getElementById("prism-height").value)
 
-  // get the hourly pay from the user
+  // get the width from user input
   let width= parseFloat(document.getElementById("prism-width").value)
 
 
-  //calculate the pay  
+  //calculate the volume 
   let volume = length*height*width;
-  //calculate the taxes
+  //calculate the sirface area
   let widthLength = 2*width*length;
   let heightLength = 2*height*length;
   let heightWidth = 2*height*width;
   let surfaceArea = widthLength+heightLength+heightWidth
 
 
-  // display the user's pay
+  // display the volume and surface area
   
   document.getElementById('answer1').innerHTML = "The volume of your rectangular prism is " +volume.toFixed(2)+"cm<sup>3</sup>";
   // display how much the government will pay
